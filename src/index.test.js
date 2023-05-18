d03a3eeededa740a0a9ddf719d1b0f0cf7c588b4
Andrey <i.gastid@gmail.com>
2023-05-18 18:12:55 +0300

    initial project commit

--- src/index.test.js
+++ src/index.test.js
@@ -4,7 +4,10 @@ describe("test getMonth function",() => {
     it("should operate correctly with numbers from one to twelve", ()=>{
         expect(getMonth(9)).toBe("сентябрь");
     }),
-    it("should operate correctly another numbers", ()=>{
-        expect(getMonth(13)).toBe("неизвестно");
+    it("should operate correctly with another numbers", ()=>{
+        expect(getMonth(13)).toBe("число находится вне диапазона от 1 до 12");
+    }),
+    it("should operate correctly with not number values", ()=>{
+        expect(getMonth('1')).toBe("Введено не число");
     })
 })
